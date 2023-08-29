f3cfc111719c5e1961e5b50747e26204c9234c3d
AhmetNishefci <an48933@ubt-uni.net>
2023-08-29 19:43:49 +0200

    fix a dynamic import

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -3,7 +3,7 @@
 import useCountries from '@/app/hooks/useCountries'
 import useSearchModal from '@/app/hooks/useSearchModal'
 import { differenceInDays } from 'date-fns'
-import { useSearchParams } from 'next/dist/client/components/navigation'
+import { useSearchParams } from 'next/navigation'
 import { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
 
